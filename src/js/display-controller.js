34af13b6d4409961b4e0d41203ec983a5774abed
Michael Sullivan <sullivanmichael344@gmail.com>
2024-08-30 00:12:17 -0400

    get images

--- src/js/display-controller.js
+++ src/js/display-controller.js
@@ -15,6 +15,54 @@ function screenController(){
     const forecast = document.querySelectorAll(".forecast-item");
     const highLowForecast = document.querySelectorAll(".high-low-forecast");
     const icons = document.querySelectorAll("img");
+    const getUrl = (icon) => {
+        if (icon === "clear-day"){
+            return clearDay;
+        } else if(icon === "clear-night"){
+            return clearNight;
+        } else if(icon === "cloudy"){
+            return cloudy;
+        } else if(icon === "fog"){
+        return fog;
+        } else if(icon === "hail"){
+            return hail;
+        } else if(icon === "partly-cloudy-day"){
+            return partlyCloudyDay;
+         } else if(icon === "partly-cloudy-night"){
+                 return partlyCloudyNight;
+         } else if(icon === "rain-snow-showers-day"){
+                 return rainSnowShowersDay;
+         } else if(icon === "rain-snow-showers-night"){
+                 return rainSnowShowersNight;
+         } else if(icon === "rain-snow"){
+                return rainSnow;
+         } else if(icon === "rain"){
+                return rain;
+         } else if(icon === "showers-day"){
+                return showersDay;
+        }else if(icon === "showers-night"){
+            return showersNight;
+        }else if(icon === "sleet"){
+            return sleet;
+        }else if(icon === "snow-showers-day"){
+            return snowShowersDay;
+        }else if(icon === "snow-showers-night"){
+            return snowShowersNight;
+        }else if(icon === "snow"){
+            return snow;
+        }else if(icon === "thunder-rain"){
+            return thunderRain;
+        }else if(icon === "thunder-showers-day"){
+            return thunderShowersDay;
+        }else if(icon === "thunder-showers-night"){
+            return thunderShowersNight;
+        }else if(icon === "thunder"){
+            return thunder;
+        }else if(icon === "wind"){
+            return wind;
+        }
+    }
+   
     const getSearchButton = () => {
         return searchButton;
     }
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
 import '../css/style.css'
 import {fetchWeatherData, consolidateWeatherData } from './weather-data';
 import {appController} from './app-controller'
+import clearDay from '../img/clear-day.png'
+import clearNight from '../img/clear-night.png'
+import cloudy from '../img/cloudy.png'
+import fog from '../img/fog.png'
+import hail from '../img/hail.png'
+import partlyCloudyDay from '../img/partly-cloudy-day.png'
+import partlyCloudyNight from '../img/partly-cloudy-night.png'
+import rainSnowShowersDay from '../img/rain-snow-showers-day.png'
+import rainSnowShowersNight from '../img/rain-snow-showers-night.png'
+import rainSnow from '../img/rain-snow.png'
+import rain from '../img/rain.png'
+import showersDay from '../img/showers-day.png'
+import showersNight from '../img/showers-night.png'
+import sleet from '../img/sleet.png'
+import snowShowersDay from '../img/snow-showers-day.png'
+import snowShowersNight from '../img/snow-showers-night.png'
+import snow from '../img/snow.png'
+import thunderRain from '../img/thunder-rain.png'
+import thunderShowersDay from '../img/thunder-showers-day.png'
+import thunderShowersNight from '../img/thunder-showers-night.png'
+import thunder from '../img/thunder.png'
+import wind from '../img/wind.png'
+
 
 
 
@@ -8,6 +31,7 @@ import {appController} from './app-controller'
     let manhattan = await consolidateWeatherData("manhattan");
     console.log(manhattan.weatherDataFahrenheit)
     console.log(manhattan.getPrecipProb());
+    console.log(clearDay)
     
     
 })();
